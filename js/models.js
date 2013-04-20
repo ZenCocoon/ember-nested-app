91d0e9c8214ac4fde35c04723f136361a1326ac9
Sébastien Grosjean <public@zencocoon.com>
2013-04-20 23:33:05 +0300

    Fix fixtures definition. Thanks ryanh@irc

--- js/models.js
+++ js/models.js
@@ -25,58 +25,55 @@ App.Rule = DS.Model.extend({
 
 App.Table.FIXTURES = [{
   id: 1,
-  name: 'Table 1'
+  name: 'Table 1',
+  seasons: [1, 2, 3],
+  rules: [1, 2]
 }, {
   id: 2,
-  name: 'Table 2'
+  name: 'Table 2',
+  seasons: [4],
+  rules: [3]
 }];
 
 App.Season.FIXTURES = [{
   id: 1,
-  table: 1,
-  name: 'Season 1'
+  name: 'Season 1',
+  periods: [1, 2, 3]
 }, {
   id: 2,
-  table: 1,
-  name: 'Season 2'
+  name: 'Season 2',
+  periods: [4]
 }, {
   id: 3,
-  table: 1,
-  name: 'Season 3'
+  name: 'Season 3',
+  periods: []
 }, {
   id: 4,
-  table: 2,
-  name: 'Season 4'
+  name: 'Season 4',
+  periods: []
 }];
 
 App.Period.FIXTURES = [{
   id: 1,
-  season: 1,
   name: 'Period 1'
 }, {
   id: 2,
-  season: 1,
   name: 'Period 2'
 }, {
   id: 3,
-  season: 1,
   name: 'Period 3'
 }, {
   id: 4,
-  season: 2,
   name: 'Period 4'
 }];
 
 App.Rule.FIXTURES = [{
   id: 1,
-  table: 1,
   name: 'Rule 1'
 }, {
   id: 2,
-  table: 1,
   name: 'Rule 2'
 }, {
   id: 3,
-  table: 2,
   name: 'Rule 3'
 }];
